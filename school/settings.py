320bf7531cb7568dc35b651523ccdf6d23c8c10a
Makimoto Masatoshi <masa526sk8@gmail.com>
2020-04-07 00:12:36 +0900

    Herokuの設定

--- school/settings.py
+++ school/settings.py
@@ -126,10 +126,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
